036d3c8b95fe15452da29cc96a20f043885b6456
thinh <dqthinh16@gmail.com>
2025-08-11 22:57:11 -0700

    powershell testing

---
+++ PowerShell/test2.ps1
@@ -0,0 +1,18 @@
+# my first comment! 
+# Write-Host "Hello World!"
+
+# Comment code =>  Verb - Noun. For example: Write (Verb) - Host(Noun). Also called "cmdlet"
+
+$PokemonCaught = '907'  
+    if ($PokemonCaught -eq 908) 
+    {Write-Host "you are a Pokemin maste.Cool Cool!"
+        } Else{
+            Write-Host "Go catch more Pokemond"
+        }
+
+$PokemonNum = 152
+if($PokemonNum -ge 0 -and $PokemonNum -le 151) {
+    Write-Host 'your pokemon is from kanto'
+} elseif ($PokemonNum -ge 252 -and -$PokemonNum -le 286                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                    xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx) {
+    <# Action when this condition is true #>
+}
